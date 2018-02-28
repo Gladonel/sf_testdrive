9286329f3bf2f99f36e53e559aad8395224971d6
Vu Minh Hai <haivm@fsoft.com.vn>
2018-02-28 15:39:19 +0700

    Add custom pipes to module to prevent error while building prod

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -4,11 +4,15 @@ import {CommonModule} from '@angular/common';
 import {SoftwareComponent} from './software.component';
 import {ServicesRoutingModule} from './services-routing.module';
 import {NgxDatatableModule} from '@swimlane/ngx-datatable';
+import {LastUsedDate} from '../pipes/siftfox.pipes';
+
+
 
 @NgModule({
   imports: [CommonModule, ServicesRoutingModule, NgxDatatableModule],
   declarations: [
-    SoftwareComponent
+    SoftwareComponent,
+    LastUsedDate
   ]
 })
 export class ServicesModule {
